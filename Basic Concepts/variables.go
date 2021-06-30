bd7dffaa6f89ad04297a7da045c8d8d96494d44a
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-07-01 01:13:46 +0300

    Update and rename a.go to variables.go

---
+++ Basic Concepts/variables.go
@@ -0,0 +1,24 @@
+Variables
+      //Variables are used to store values.
+
+      //In Go, the var keyword is used to declare variables.
+      //For example:
+var i int
+
+      //The code above declares a variable named i of type int.
+      //int stands for integer and represents whole numbers.
+
+      //We can assign the variable a value and output it:
+package main
+
+import "fmt"
+
+func main() {
+  var i int = 8
+  fmt.Println(i) 
+} //output: 8
+
+      //We will learn about the other variable types that Go supports in the next lesson.
+
+
+
