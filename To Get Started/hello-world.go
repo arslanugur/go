86c4739360021d6908d14948459a97aa4b45d748
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-06-30 23:45:39 +0300

    Update and rename a.go to hello-world.go

---
+++ To Get Started/hello-world.go
@@ -0,0 +1,34 @@
+Hello golang.org 
+      /*Go is an open source programming language that was designed at Google in 2007.
+        to improve programming productivity in an era of multicore, networked machines and large codebases.*/ 
+
+      /*
+        Go was used to develop Docker and Kubernetes. 
+        Developed by Google engineer Rob Pike who's also known as Unix guru. 
+        It's community-led and open source. It's a C-like language. 
+        it provides automatic memory management including garbage collection. 
+        Go is really fast for it's compiled to machine code.
+      */
+      /*Go is best for developing 
+             Distributed networked services, Cloud-native Development, 
+             Replacements for existing infrastructure, Utilities and stand-alone tools. */
+
+      //Let's start with a simple program that outputs "Hello, World!" to the screen.
+      //Example:
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, World!") //fmt - format
+}
+
+       /*
+       This might seem like a lot of code for just a simple output, 
+       but we will break it down and explain each of the lines of code 
+       and what each statement is responsible for.*/ 
+
+      //https://golang.org/pkg/fmt/
+      //https://gobyexample.com/
+
+
